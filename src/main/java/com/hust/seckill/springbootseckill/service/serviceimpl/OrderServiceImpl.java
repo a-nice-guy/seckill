bcedfce95ea82fa1d2fa75d2fa2137dd3a79cfa5
Mr Sun <1443455198@qq.com>
2022-12-26 23:59:48 +0800

    最终基础版

--- src/main/java/com/hust/seckill/springbootseckill/service/serviceimpl/OrderServiceImpl.java
+++ src/main/java/com/hust/seckill/springbootseckill/service/serviceimpl/OrderServiceImpl.java
@@ -54,16 +54,16 @@ public class OrderServiceImpl implements OrderService {
             throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR,"数量信息不正确");
         }
 
-//        //校验活动信息
-//        if(promoId != null){
-//            //（1）校验对应活动是否存在这个适用商品
-//            if(promoId.intValue() != itemModel.getPromoModel().getId()){
-//                throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR,"活动信息不正确");
-//                //（2）校验活动是否正在进行中
-//            }else if(itemModel.getPromoModel().getStatus().intValue() != 2) {
-//                throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR,"活动信息还未开始");
-//            }
-//        }
+        //校验活动信息
+        if(promoId != null){
+            //（1）校验对应活动是否存在这个适用商品
+            if(promoId.intValue() != itemModel.getPromoModel().getId()){
+                throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR,"活动信息不正确");
+                //（2）校验活动是否正在进行中
+            }else if(itemModel.getPromoModel().getStatus().intValue() != 2) {
+                throw new BusinessException(EmBusinessError.PARAMETER_VALIDATION_ERROR,"活动信息还未开始");
+            }
+        }
 
         //2.落单减库存
         boolean result = itemService.decreaseStock(itemId,amount);
@@ -76,11 +76,11 @@ public class OrderServiceImpl implements OrderService {
         orderModel.setUserId(userId);
         orderModel.setItemId(itemId);
         orderModel.setAmount(amount);
-//        if(promoId != null){
-//            orderModel.setItemPrice(itemModel.getPromoModel().getPromoItemPrice());
-//        }else{
-//            orderModel.setItemPrice(itemModel.getPrice());
-//        }
+        if(promoId != null){
+            orderModel.setItemPrice(itemModel.getPromoModel().getPromoItemPrice());
+        }else{
+            orderModel.setItemPrice(itemModel.getPrice());
+        }
         orderModel.setPromoId(promoId);
         orderModel.setOrderPrice(orderModel.getItemPrice().multiply(new BigDecimal(amount)));
 
