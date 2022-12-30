202d04ab3830164454e884671957dc5192af4325
Mr Sun <1443455198@qq.com>
2022-12-30 16:06:50 +0800

    定制化内嵌tomcat解决并发问题

---
+++ src/main/java/com/hust/seckill/springbootseckill/config/WebServerConfiguration.java
@@ -0,0 +1,28 @@
+package com.hust.seckill.springbootseckill.config;
+
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.coyote.http11.Http11NioProtocol;
+import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.ConfigurableWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.stereotype.Component;
+
+//当spring中没有对应bean时，将此bean加载至spring中
+@Component
+public class WebServerConfiguration implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
+    @Override
+    public void customize(ConfigurableWebServerFactory factory) {
+        ((TomcatServletWebServerFactory)factory).addConnectorCustomizers(new TomcatConnectorCustomizer() {
+            @Override
+            public void customize(Connector connector) {
+                Http11NioProtocol protocol = (Http11NioProtocol) connector.getProtocolHandler();
+                //设置keepalive长连接时间为30s
+                protocol.setKeepAliveTimeout(30000);
+                //设置最大请求次数为10000
+                protocol.setMaxKeepAliveRequests(10000);
+            }
+        });
+    }
+}
